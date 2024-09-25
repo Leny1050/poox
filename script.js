a65f5e04d1026581dba6f7565ab03884f6c4700e
Leny1050 <101010tatata1010@gmail.com>
2024-09-25 22:09:18 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ window.addEventListener('load', function() {
     const buttonContainer = document.getElementById('button-container');
     const popupContainer = document.getElementById('popup-container');
 
-    // Массив со звуковыми файлами
     const sounds = [
         "https://cdn.freesound.org/previews/721/721532_13999124-lq.mp3",
         "https://cdn.freesound.org/previews/572/572499_12522405-lq.mp3", 
@@ -14,24 +13,21 @@ window.addEventListener('load', function() {
         "https://cdn.freesound.org/previews/671/671202_13659822-lq.mp3"
     ];
 
-    // Обработчик клика на кнопку
-    button.addEventListener('click', function() {
-        // Скрываем кнопку
-        buttonContainer.style.display = 'none';
+    button.addEventListener('click', playSound);
+    button.addEventListener('touchstart', playSound);
 
-        // Показываем контейнер с изображением
+    function playSound() {
+        buttonContainer.style.display = 'none';
         popupContainer.style.display = 'block';
 
-        // Выбор случайного звука
         const randomIndex = Math.floor(Math.random() * sounds.length);
-        sound.src = sounds[randomIndex]; // Устанавливаем случайный звук
+        sound.src = sounds[randomIndex];
 
-        // Резкое появление изображения и воспроизведение звука
         setTimeout(() => {
             image.style.opacity = 1;
             sound.play().catch((error) => {
                 console.log('Ошибка при воспроизведении звука:', error);
             });
-        }, 100); // Задержка перед появлением (100 мс)
-    });
+        }, 100);
+    }
 });
