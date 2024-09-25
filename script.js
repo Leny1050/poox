0f32f694b038b047c6c148568b3bc4757253f999
Leny1050 <101010tatata1010@gmail.com>
2024-09-25 10:29:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
 window.addEventListener('load', function() {
     const image = document.getElementById('popup-image');
     const sound = document.getElementById('popup-sound');
+    const text = document.getElementById('popup-text');
+    let volume = 0;
 
-    // Воспроизводим звук
+    // Постепенное увеличение громкости звука
+    sound.volume = volume;
     sound.play();
+
+    const volumeInterval = setInterval(() => {
+        if (volume < 1) {
+            volume += 0.05;
+            sound.volume = volume;
+        } else {
+            clearInterval(volumeInterval);
+        }
+    }, 200); // Каждые 200 мс увеличиваем громкость
+
+    // Плавное появление текста
+    setTimeout(() => {
+        text.style.opacity = 1;
+    }, 2000);
 
     // Плавное появление и начало мигания изображения
     setTimeout(() => {
         image.style.transform = 'scale(1)';
-    }, 100); // Небольшая задержка для эффекта выскакивания
+        image.style.animation = 'shake 0.2s infinite'; // Эффект дрожания
+    }, 100);
 });
