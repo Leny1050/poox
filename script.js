668895ed214fd93c38429e5fd86cb2f8043cb57d
Leny1050 <101010tatata1010@gmail.com>
2024-09-25 11:00:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 window.addEventListener('load', function() {
-    const image = document.getElementById('popup-image');
-    const sound = document.getElementById('popup-sound');
+    const image = document.getElementById('popup-image');
+    const sound = document.getElementById('popup-sound');
 
-    // Изображение плавно появляется через 0.5 секунд
-    setTimeout(() => {
-        image.style.opacity = 1;
-    }, 500);
+    // Изображение плавно появляется через 0.5 секунд
+    setTimeout(() => {
+        image.style.opacity = 1;
+    }, 500);
 
-    // Воспроизведение звука после появления изображения
-    image.addEventListener('transitionend', function() {
-        sound.play().catch((error) => {
-            console.log('Ошибка при воспроизведении звука:', error);
-        });
-    });
+    // Воспроизведение звука после появления изображения
+    image.addEventListener('transitionend', function() {
+        sound.play().catch((error) => {
+            console.log('Ошибка при воспроизведении звука:', error);
+        });
+    });
 });
