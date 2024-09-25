a30cafec9442177c15853cb5c213810e2700a805
Leny1050 <101010tatata1010@gmail.com>
2024-09-25 21:57:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,15 @@ window.addEventListener('load', function() {
     const buttonContainer = document.getElementById('button-container');
     const popupContainer = document.getElementById('popup-container');
 
+    // Массив со звуковыми файлами
+    const sounds = [
+        "https://cdn.freesound.org/previews/721/721532_13999124-lq.mp3",
+        "https://cdn.freesound.org/previews/572/572499_12522405-lq.mp3", 
+        "https://cdn.freesound.org/previews/104/104039_179538-lq.mp3",
+        "https://cdn.freesound.org/previews/239/239901_4079949-lq.mp3",
+        "https://cdn.freesound.org/previews/671/671202_13659822-lq.mp3"
+    ];
+
     // Обработчик клика на кнопку
     button.addEventListener('click', function() {
         // Скрываем кнопку
@@ -12,6 +21,10 @@ window.addEventListener('load', function() {
 
         // Показываем контейнер с изображением
         popupContainer.style.display = 'block';
+
+        // Выбор случайного звука
+        const randomIndex = Math.floor(Math.random() * sounds.length);
+        sound.src = sounds[randomIndex]; // Устанавливаем случайный звук
 
         // Резкое появление изображения и воспроизведение звука
         setTimeout(() => {
